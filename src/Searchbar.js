81c34333edf77c52f74e61680a14fcb198927c47
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-30 18:56:21 +0200

    fix form submittion

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -7,6 +7,7 @@ export default function Searchbar({search, setSearch, setLoading}) {
     // creating handle search func to add searched words to the api
     function handleSearchbarChange (e) {
         // asign value of input to variable
+        e.preventDefault()
         setLoading(true)
         const data = searchInput.current.value
         if (data === '') return
@@ -27,6 +28,7 @@ export default function Searchbar({search, setSearch, setLoading}) {
     ) */
 
     return (
+        <form onSubmit={handleSearchbarChange}>
         <div style={{display: "flex"}} >
             <input type='text'
                 placeholder='Type here to search...'
@@ -34,5 +36,6 @@ export default function Searchbar({search, setSearch, setLoading}) {
             </input>
             <button onClick={handleSearchbarChange}>&#x1F50D;</button>
         </ div>
+        </form>
     )
 }
